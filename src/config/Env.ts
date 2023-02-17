cbc5c9f84f90e4b930f4e1e6f837006107620f55
Bamboo <75402531+Shiou-Ju@users.noreply.github.com>
2023-02-17 16:07:13 +0800

    feat: add open api key as env

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -3,10 +3,12 @@ import { logger } from '..';
 
 dotenv.config();
 
-const { NODE_ENV, PORT, MONGO_URL, JWT_SECRET } = process.env;
+const { NODE_ENV, PORT, MONGO_URL, JWT_SECRET, OPENAI_API_KEY } = process.env;
 
-if (!MONGO_URL || !JWT_SECRET || !NODE_ENV) {
-  logger.error('Must provide env: MONGO_URL, JWT_SECRET , NODE_ENV');
+if (!MONGO_URL || !JWT_SECRET || !NODE_ENV || !OPENAI_API_KEY) {
+  logger.error(
+    'Must provide env: MONGO_URL, JWT_SECRET , NODE_ENV, OPENAI_API_KEY'
+  );
   process.exit();
 }
 
@@ -14,3 +16,4 @@ export const mongoUrl = MONGO_URL;
 export const port = PORT || 5000;
 export const jwtSecret = JWT_SECRET;
 export const nodeEnv = NODE_ENV;
+export const openAIKey = OPENAI_API_KEY;
