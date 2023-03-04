c5f5b7999b141676618cb20ba16ecccc412c6440
Bamboo <75402531+Shiou-Ju@users.noreply.github.com>
2023-03-04 21:02:11 +0800

    feat: use official package to implement example

--- src/controllers/gpt.ts
+++ src/controllers/gpt.ts
@@ -1,13 +1,22 @@
-import { ChatGPTAPI } from 'chatgpt';
 import { openAIKey } from '../config/Env';
+import { Configuration, OpenAIApi } from 'openai';
 
-const api = new ChatGPTAPI({
+const configuration = new Configuration({
   apiKey: openAIKey,
 });
-
-const text = 'Please tell me more about chatgpt!';
+const openai = new OpenAIApi(configuration);
 
 export const example = async () => {
-  const res = await api.sendMessage(text);
-  console.log(res.text);
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content:
+          '用繁體中文回答我，我們要怎麼進步？',
+      },
+    ],
+  });
+  console.log(JSON.stringify(completion.data));
+  console.log(completion.data);
 };
